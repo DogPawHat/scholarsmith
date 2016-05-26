1b616a738dd49103576b7a2f452f7b35e3ba3890
DogPawHat <ciaran.curley.11.11@gmail.com>
2016-05-26 23:05:12 +0100

    Build on the package

--- build.ts
+++ build.ts
@@ -1,6 +1,42 @@
 import Marked from "marked";
 import Handlebars from "handlebars";
+import fm from "yaml-front-matter";
 import fs from "fs";
+import path from "path";
 
-Handlebars.registerPartial("head", "templates/partials/head.hbs");
-Handlebars.registerPartial
+interface contextStatic{
+    pages: {}[]
+}
+
+for(let file of fs.readdirSync("templates/partials")){
+    let partialName = file.split(".")[0];
+    Handlebars.registerPartial(
+        partialName,
+        fs.readFileSync(path.join(
+                "templates",
+                "partials",
+                file
+            )
+        )
+    );
+}
+
+let context: contextStatic;
+let pages: {}[];
+for(let file of fs.readdirSync("tutorial")){
+    let contentObj = fm.loadFront(
+        fs.readFileSync(path.join(
+            "tutorial",
+            file
+        ))
+    );
+
+    contentObj.__content = marked.parse(contentObj.__content);
+
+    context.pages.push(contentObj);
+}
+
+
+let indexTemplate = Handlebars.compile(fs.readFileSync("templates/index.hbs"));
+
+let indexHtml = indexTemplate(context)
