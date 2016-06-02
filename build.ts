cd835845d183bc11c86febb33c83f0001565b783
DogPawHat <ciaran.curley.11.11@gmail.com>
2016-06-02 22:37:13 +0100

    Further refinements

--- build.ts
+++ build.ts
@@ -6,44 +6,47 @@ import * as fs from "fs";
 import * as path from "path";
 
 function getDirectories(srcpath) {
-  return fs.readdirSync(srcpath).filter(function(file) {
-    return fs.statSync(path.join(srcpath, file)).isDirectory();
-  });
+    return fs.readdirSync(srcpath).filter(function(file) {
+        return fs.statSync(path.join(srcpath, file)).isDirectory();
+    });
 }
 
-function parseTopics(){
+function parseTopics() {
     let topics = [];
-    for(let dir of getDirectories("tutorial/topics")){
+    for (let dir of getDirectories("tutorial/topics")) {
         console.log(path.join("tutorial",
-        "topics",
-        dir,
-        "config.yaml"));
+            "topics",
+            dir,
+            "config.yaml"));
         let pages = [];
         let configFile = fs.readFileSync(
             path.join("tutorial",
-            "topics",
-            dir,
-            "config.yaml")
+                "topics",
+                dir,
+                "config.yaml")
         ).toString();
         let configYaml = yaml.safeLoad(configFile);
 
 
 
-        for(let file of fs.readdirSync(path.join(
+        for (let file of fs.readdirSync(path.join(
             "tutorial", "topics", dir
-        ))){
-            let contentObj = fm.loadFront(
-                fs.readFileSync(path.join(
-                    "tutorial",
-                    "topics",
-                    dir,
-                    file
-                ))
-            );
+        ))) {
+            if (file !== "config.yaml") {
+                let contentObj = fm.loadFront(
+                    fs.readFileSync(path.join(
+                        "tutorial",
+                        "topics",
+                        dir,
+                        file
+                    ))
+                );
+
 
             contentObj.markedContent = marked(contentObj.__content);
 
             pages.push(contentObj);
+            }
         }
 
 
@@ -55,15 +58,15 @@ function parseTopics(){
     return topics
 }
 
-for(let file of fs.readdirSync("templates/partials")){
+for (let file of fs.readdirSync("templates/partials")) {
     let partialName = file.split(".")[0];
     handlebars.registerPartial(
         partialName,
         fs.readFileSync(path.join(
-                "templates",
-                "partials",
-                file
-            )
+            "templates",
+            "partials",
+            file
+        )
         ).toString()
     );
 }
