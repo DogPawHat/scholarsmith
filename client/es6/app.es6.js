b8efeb0c1069499f362961449742439955f1a48a
DogPawHat <ciaran.curley.11.11@gmail.com>
2016-07-12 17:30:45 +0100

    Do not build, I repeat, do not build.

--- client/es6/app.es6.js
+++ client/es6/app.es6.js
@@ -3,6 +3,7 @@ import '../scss/app.scss';
 import $ from 'jquery';
 import foundation from 'foundation-sites/dist/foundation.js';
 import {attachSubmitHandlers} from "./test.es6.js";
+import selectPage from "./pager.es6.js";
 
 $(document).ready(function(){
     var pager = $(".content ul#pageselect");
---
+++ client/es6/pager.es6.js
@@ -0,0 +1,25 @@
+export default function(pager, topics, pages){
+    const clickNumber = (e) => {
+        let value = e.target.value;
+        pages.removeClass("current");
+        pager.children().removeClass("current");
+        pager.eq(value).addClass("current");
+        topics.eq(value).addClass("current");
+    }
+
+    const clickLeftArrow = () => {
+        let current = pages.find(".current");
+
+        current.removeClass("current");
+        pager.children().removeClass("current");
+    }
+
+    const clickRightArrow = () => {
+
+    }
+
+    let arrows = pager.children(".arrow")
+    arrows.eq(0).click(clickLeftArrow);
+    arrows.eq(1).click(clickRightArrow);
+    pager.children(":not(.arrow)").click(clickNumber);
+}
