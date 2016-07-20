6554d06a1590d086ce0a2953d1225d9fb5b97a75
DogPawHat <ciaran.curley.11.11@gmail.com>
2016-07-20 17:26:08 +0100

    Considering the nature of state

--- templates/PageSelect.tsx
+++ templates/PageSelect.tsx
@@ -5,8 +5,21 @@ interface PageSelectState{
     currentPage: number;
 }
 
+interface PageLinkProps{
+    key: React.Key;
+    changePage: () => void;
+}
 
-class Clicker 
+interface PageLinkState{
+    current: boolean;
+}
+
+function PageLink(props: PageLinkProps): React.ReactElement<PageLinkProps>{
+    const setCurrent = () => {
+        this.props.changePage(props.key);
+    }
+    return <li><a onClick={setCurrent} key={props.key}>{props.key}</a></li>;
+}
 
 
 export default class extends React.Component<PageSelectProps, PageSelectState> {
@@ -22,12 +35,13 @@ export default class extends React.Component<PageSelectProps, PageSelectState> {
     }
 
     render (){
+        return(
         <ul id="pageselect">
             <li className="arrow"><a href="#">&laquo; </a></li>
             {this.props.topics.map((topic, i) => {
-                <li><a onClick={this.changeCurrentPage} key={i}>{i}</a></li>
+                <PageLink key={i} changePage={this.changeCurrentPage.bind(this)}/>
             })}
             <li className="arrow"><a href="#">&raquo; </a></li>
-        </ul>
+        </ul>);
     }
 }
--- templates/Topics.tsx
+++ templates/Topics.tsx
@@ -2,35 +2,41 @@ import React from 'react';
 import Marked from 'marked';
 import {PageProps, TopicProps, TopicsProps} from './TemplateProps';
 
-function Page(props: PageProps): React.ReactElement<PageProps>{
-    return(
-        <article class="page" dangerouslySetInnerHTML={Marked(props.text)} />
+function Page(props: PageProps): React.ReactElement<PageProps> {
+    return (
+        <article class="page" dangerouslySetInnerHTML={Marked(props.text) } />
     );
 }
 
 function Topic(props: TopicProps): React.ReactElement<TopicProps> {
     let pageElements = props.pages.map((page, i) => {
-                return Page(page);
-            })
+        return Page(page);
+    })
     return (
         <article className="topic">
             <h2 className="page">{ props.title }</h2>
-            {pageElements} 
+            {pageElements}
         </article>
     )
 }
 
-export default function Topics(props: TopicsProps) {
-    return (
-        <div className="topics">
-            <h1 className="page current">WELCOME</h1>
-            {props.topics.map((topic) => {
-                return Topic(topic);
-            })}
-            {{{ tutorialtest }}}
-            <div className="page talk_to_us">
-                <p>Please Talk to us</p>
+interface TopicsState{
+    current: number;
+}
+
+export default class Topics extends React.Component<TopicProps, TopicsState>{
+    render(){
+        return (
+            <div className="topics">
+                <h1 className="page current">WELCOME</h1>
+                {this.props.topics.map((topic) => {
+                    return Topic(topic);
+                }) }
+                {{{ tutorialtest }}}
+                <div className="page talk_to_us">
+                    <p>Please Talk to us</p>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
