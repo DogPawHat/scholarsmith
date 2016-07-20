8783420c3bb03f497f8565e2274278869efe33e1
DogPawHat <ciaran.curley.11.11@gmail.com>
2016-07-20 15:22:19 +0100

    Saving unfinished work

--- templates/PageSelect.tsx
+++ templates/PageSelect.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import {PageSelectProps} from './TemplateProps';
 
-function PageSelect(props: PageSelectProps) {
-    return (
+interface PageSelectState{
+    currentPage: number;
+}
+
+class PageSelect extends React.Component<PageSelectProps, PageSelectState> {
+    constructor(){
+        super();
+        this.state = {
+            currentPage: 0
+        };
+    }
+
+    changeCurrentPage(i: number){
+        this.state.currentPage = i;
+    }
+
+    render (){
         <ul id="pageselect">
             <li className="arrow"><a href="#">&laquo; </a></li>
-            {props.topics.map((topic, i) => {
-                <li><a href="#">{i}</a></li>
+            {this.props.topics.map((topic, i) => {
+                <li><a onClick={this.changeCurrentPage} key={i}>{i}</a></li>
             })}
             <li className="arrow"><a href="#">&raquo; </a></li>
         </ul>
-    )
+    }
 }
