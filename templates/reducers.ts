26e05245f7e35974dede92b71f7a546593797001
DogPawHat <ciaran.curley.11.11@gmail.com>
2016-08-05 11:40:28 +0100

    Quite Please with myself

--- templates/reducers.ts
+++ templates/reducers.ts
@@ -1,20 +1,39 @@
-import Redux from 'redux';
+import {Reducer, Action, combineReducers} from 'redux';
 import Immutable from 'immutable';
-import actions from './actions';
+import { NEXT_PAGE, PREV_PAGE, SELECT_TOPIC, SelectTopicAction} from './actions';
 
-interface TutoralState extends Immutable.Map<string, any> {
-    currentPage: number,
-    currentScore: number
+type TutoralStateType = Immutable.Map<string, any>;
+
+const CURRENT_PAGE = 'CURRENT_PAGE';
+const CURRENT_SCORE = 'CURRENT_SCORE'
+
+const initialCurrentState = Immutable.Map<string, any>([
+    [CURRENT_PAGE, 0],
+    [CURRENT_SCORE, 0]
+]);
+
+const currentPageHandlers =
+    Immutable.Map<string, Reducer<TutoralStateType>>([
+        [NEXT_PAGE, (state: TutoralStateType, action: Action) => {
+            return state.set(CURRENT_PAGE, state.get(CURRENT_PAGE) + 1)
+        }],
+        [PREV_PAGE, (state: TutoralStateType, action: Action) => {
+            return state.set(CURRENT_PAGE, state.get(CURRENT_PAGE) - 1);
+        }],
+        [SELECT_TOPIC, (state: TutoralStateType, action: SelectTopicAction) => {
+            return state.set(CURRENT_PAGE, action.topic_id);
+        }]
+    ]);
+
+const currentPage: Reducer<TutoralStateType> = (state: TutoralStateType, action: Action) => {
+    return currentPageHandlers.get(action.type)(state, action);
+};
+
+const currentScore: Reducer<TutoralStateType> = (state: TutoralStateType, action: Action) => {
+    return state;
 }
 
-const goToNextPage: Redux.Reducer<Immutable.Map<string, any>> = function (state:TutoralState, action: Redux.Action) {
-    return state.set("currentPage", state.currentPage + 1);
-};
-
-const goToPrevPage: Redux.Reducer<Immutable.Map<string, any>> = function (state:TutoralState, action: Redux.Action) {
-    return state.set("currentPage", state.currentPage - 1);
-};
-
-const setTopicId: Redux.Reducer<Immutable.Map<string, any>> = function (state:TutoralState, action: actions.SelectTopicAction) {
-    return state.set("currentPage", action.topic_id);
-};
+const reducers: Reducer<TutoralStateType> = combineReducers<TutoralStateType>({
+    CURRENT_PAGE: currentPage,
+    CURRENT_SCORE: currentScore
+})
