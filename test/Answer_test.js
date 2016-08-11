453d2a6c9b468b439a726a8ddfd31e096fad8a83
DogPawHat <ciaran.curley.11.11@gmail.com>
2016-08-11 17:20:49 +0100

    It's working?

--- test/Answer_test.js
+++ test/Answer_test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import Answer from './test_lib/templates/presentation/Answer';
+import Answer from '../src/templates/presentation/Answer';
 
 test((t) => {
     const wrapper = shallow(<Answer index={1} value='hello'/>);
