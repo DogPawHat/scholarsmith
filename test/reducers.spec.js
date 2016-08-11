ea37bc871a3aefb49c1a8fe035d7c7cdf1954343
DogPawHat <ciaran.curley.11.11@gmail.com>
2016-08-11 21:23:43 +0100

    Free Muffins are nice

--- test/reducers.spec.js
+++ test/reducers.spec.js
@@ -2,7 +2,43 @@ import test from 'ava';
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import reducers from '../src/templates/reducers';
+import {NEXT_PAGE, PREV_PAGE, SET_PAGE} from '../src/templates/actions';
 
 test(async (t) => {
-    await t.is(true, false)
+    const oldState = {
+        COURSE_DATA: {
+            title: '',
+            pages: []
+        },
+        CURRENT_PAGE: 0,
+        CURRENT_SCORE: 0
+    }
+
+    const expectedState = {
+        COURSE_DATA: {
+            title: '',
+            pages:[]
+        },
+        CURRENT_PAGE: 1,
+        CURRENT_SCORE: 0
+    }
+
+    const unExpectedState = {
+        COURSE_DATA: {
+            title: '',
+            pages:[]
+        },
+        CURRENT_PAGE: 0,
+        CURRENT_SCORE: 1
+    }
+
+    const action = {
+        type: NEXT_PAGE
+    }
+
+    const newState = reducers(oldState, action);
+
+    t.not(newState.CURRENT_SCORE, unExpectedState.CURRENT_SCORE);
+    await t.is(newState.CURRENT_PAGE, expectedState.CURRENT_PAGE);
+
 })
