683ac83d068777e109044deaf618a84658f54628
DogPawHat <ciaran.curley.11.11@gmail.com>
2016-08-12 10:29:50 +0100

    Moar tests nyan

--- test/actions.spec.js
+++ test/actions.spec.js
@@ -3,23 +3,23 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import {NEXT_PAGE, PREV_PAGE, SET_PAGE, createNextPageAction, createPrevPageAction, createSetPageAction} from '../src/templates/actions';
 
-test(async (t) => {
+test('should return action of type NEXT_PAGE', async (t) => {
     const wrapper = createNextPageAction();
     await t.is(wrapper.type, NEXT_PAGE);
 });
 
-test(async (t) => {
+test('should return action of type PREV_PAGE', async (t) => {
     const wrapper = createPrevPageAction();
     await t.is(wrapper.type, PREV_PAGE);
 });
 
-test(async (t) => {
+test('should return action of type SET_PAGE with new_page of 2', async (t) => {
     const wrapper = createSetPageAction(2);
     t.is(wrapper.type, SET_PAGE);
     await t.is(wrapper.new_page, 2);
 });
 
-test(async (t) => {
+test('should return action of type SET_PAGE with new_page of 15', async (t) => {
     const wrapper = createSetPageAction(15);
     t.is(wrapper.type, SET_PAGE);
     await t.is(wrapper.new_page, 15);
